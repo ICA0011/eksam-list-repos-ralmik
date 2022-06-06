12cdd468e77b4a40c70789fbeaf126bbd2f4df19
Ralf Mikk <ralmik@ttu.ee>
2022-06-06 13:38:55 +0300

    Eksam skript, mis tagastab GitHub kasutaja avaliku repode arvu

--- retrieve_repos.py
+++ retrieve_repos.py
@@ -3,6 +3,11 @@ from github import Github
 
 
 def retrieve_repos(username):
+    """
+    Requests ja getiga saan json formaadis info.
+
+    Muudan selle dictionaryks ja otsin tagastan sellest repde arvu.
+    """
     json_content = requests.get(f"https://api.github.com/users/{username}")
     conent_dict = json.loads(json_content.content)
     return int(conent_dict["public_repos"])
